3903f9672fa580eee415c5950b37e32242a8db2b
Keerthana <kumarankeerthana99@gmail.com>
2026-07-28 09:42:28 -0400

    Created JSON Reader Utility for reading the login API Test Data

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -7,9 +7,11 @@ import java.util.List;
 import org.testng.annotations.DataProvider;
 
 import com.api.request.model.CreateJobPayload;
+import com.api.request.model.UserCredentials;
 import com.api.utils.CreateJobBeanMapper;
 import com.api.utils.CsvReaderUtil;
 import com.api.utils.FakerDataGenerator;
+import com.api.utils.JsonReaderUtil;
 import com.dataproviders.bean.CreateJobBean;
 import com.dataproviders.bean.UserBean;
 
@@ -23,6 +25,12 @@ public class DataProviderUtils {
 	@DataProvider(name = "LoginAPIDataProvider", parallel = true)
 	public static Iterator<UserBean> loginAPIDataProvider() {
 		return CsvReaderUtil.loadCsv("TestData/LoginCreds.csv", UserBean.class);
+	}
+	
+	
+	@DataProvider(name = "LoginAPIJSONDataProvider", parallel = true)
+	public static Iterator<UserCredentials> loginAPIJSONDataProvider() {
+		return JsonReaderUtil.loadJSON("TestData/LoginCredsJSON.json.json", UserCredentials[].class);
 	}
 
 	@DataProvider(name = "CreateJobDataProvider", parallel = true)
