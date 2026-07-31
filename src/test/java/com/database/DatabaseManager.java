706ae8eea85fea3d02747de4cf54cbf5da2d5cb9
Keerthana <kumarankeerthana99@gmail.com>
2026-07-30 21:06:59 -0400

    Added HikariCp in the automation framework

---
+++ src/test/java/com/database/DatabaseManager.java
@@ -0,0 +1,40 @@
+package com.database;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.api.utils.ConfigManager;
+
+public class DatabaseManager  {
+	
+	private static final String DB_URL = 	ConfigManager.getProperty("DB_URL");
+	private static final String DB_USER_NAME = ConfigManager.getProperty("DB_USER_NAME");
+	private static final String DB_PASSWORD = ConfigManager.getProperty("DB_PASSWORD");
+	private static volatile Connection conn ;
+	 
+	private DatabaseManager() {
+		//private constructor to restrict object creation outside the class 
+	}
+	
+	
+
+	public static  void createConnection() throws SQLException, IOException {
+		
+		
+		if(conn ==null) {//double checked lock-in pattern
+		if (conn ==null) {
+		conn = DriverManager.getConnection(DB_URL,DB_USER_NAME ,
+				DB_PASSWORD);
+		System.out.println(conn);
+		
+		
+		}
+		}
+		
+		
+		
+	}
+
+}
---
+++ src/test/java/com/database/DemoRunner.java
@@ -0,0 +1,23 @@
+package com.database;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class DemoRunner {
+
+	public static void main(String[] args) throws SQLException, IOException {
+		
+		DatabaseManager.createConnection();
+		long start_time = System.currentTimeMillis();
+		
+	for(int i=1;i<400;i++) {
+		DatabaseManager.createConnection();
+		
+	}
+		
+		long end_time = System.currentTimeMillis();
+		System.out.println(end_time -start_time);
+
+	}
+
+}
